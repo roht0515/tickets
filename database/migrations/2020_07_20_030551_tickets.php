840cd611e9a37dbf8f1f74f6628c08f7479d4b4e
roht0515 <rodrigo051599@gmail.com>
2020-07-19 23:48:28 -0400

    first commit

---
+++ database/migrations/2020_07_20_030551_tickets.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->date('fecha');
+            $table->dateTime('hora');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('type_client_id')->constrained('type_client');
+            $table->foreignId('state_id')->constrained('states');
+            $table->foreignId('service_id')->constrained('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
